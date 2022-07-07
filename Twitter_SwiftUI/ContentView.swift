0a1211a497e12bda5f20ca9bb58358f722656743
Sai Lakshmi <sailakshmi22.p@gmail.com>
2022-07-07 15:50:51 -0400

    Initial Commit

---
+++ Twitter_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Twitter_SwiftUI
+//
+//  Created by Sai Lakshmi on 7/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Twitter_SwiftUI/Twitter_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_SwiftUIApp.swift
+//  Twitter_SwiftUI
+//
+//  Created by Sai Lakshmi on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
